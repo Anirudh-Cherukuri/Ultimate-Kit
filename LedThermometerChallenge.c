28d8864b4879326578a369b08e8c0219d03146fc
Anirudh Cherukuri <Anirudh@Sumans-MacBook-Pro-3.local>
2020-04-24 10:13:04 -0700

    Just like the LED Thermometer, but once the number finishes, it will come again after two seconds

---
+++ LedThermometerChallenge.c
@@ -0,0 +1,57 @@
+
+/*
+ * Display the temperature in LED blinks by counting out the
+ *  tens and ones place to LEDs.
+ */
+
+void setup() {
+  //Configure the LED pins
+  pinMode(5,OUTPUT);//Tens place of the temperature
+  pinMode(6,OUTPUT);//Ones place of the temperature
+
+  //Check the temp sensor pinout, getting it backwards ruins it
+  pinMode(A3,INPUT); //Center pin of temp sensor to A3
+}
+
+//copy pasted rest into loop
+void loop() {
+  /*
+   * Conversion constants 0.88 and -58 turn the analog reading 
+   *  to Farenheit. Cast analogRead(A3) to a float
+   */
+  const int tempF = int(0.88*float(analogRead(A3) - 58.0));
+
+  //Use modulo to remove the ones place from tempF.
+  //Divide difference by 10 for the tens place of tempF
+  //e.g. tempF = 35 gives (35 - 5)/10 = 3
+  const int tempTens = (tempF-(tempF%10))/10;
+
+  //Blink the 10s place digit on pin 5
+  for (int i = 0;i < tempTens;i++){
+    digitalWrite(5,HIGH);
+    delay(300);
+    digitalWrite(5,LOW);
+    delay(300);
+  }
+  
+  //Blink the 1s place digit on pin 6
+  //tempF%10 will equal the remainder of tempF / 10
+  for (int i = 0;i < tempF%10;i++){
+    digitalWrite(6,HIGH);
+    delay(300);
+    digitalWrite(6,LOW);
+    delay(300);
+
+  }
+  //added delay to separate total count
+  delay(2000);
+    
+}
+
+
+//Try to bring the calculations and blinking into loop, so that it
+//  measures repeatedly
+
+// (c) 2017 Let's Start Coding. License: www.letsstartcoding.com/bsdlicense
+
+
