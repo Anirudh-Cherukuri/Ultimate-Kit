60da1a95ff53b4268839dedc8415cfbe7d1085be
Anirudh Cherukuri <Anirudh@Sumans-MacBook-Pro-3.local>
2020-05-12 13:34:06 -0700

    Bounce a pixel across half an LED strip, as it changes color eaxh time it reaches the end of half the strip.

---
+++ LedStripChangingColorPixelBounceChallenge.c
@@ -0,0 +1,51 @@
+
+/*
+ * Bouncing a pixel up and down the LED Strip, changing color along the way
+ */
+
+#include "LEDStrip.h"
+
+const byte numPixels = 15; //number of pixels on a strip
+
+int color = 200; //the initial color value (ranges from 0-300)
+
+/*
+ * Make the LED strip object LEDStrip strip
+ *  = LEDStrip(numPixels, dataPin, clockPin);
+ */
+LEDStrip strip = LEDStrip(numPixels, 13, 12);
+
+int pixel = 0; //Position of the pixel on the strip
+
+int Direction = 1; //Direction of travel of the pixel
+
+void setup() {
+
+}
+
+void loop() {
+
+  strip.setPixel(pixel,-1); //Clear the old pixel
+  
+  pixel = pixel + Direction; //Move the pixel
+
+  strip.setPixel(pixel,color); //Set the new position and color
+
+  strip.draw(); //Draw what you set up with .setPixel
+
+
+  //If at either end of the strip
+  if ((pixel == 0)||(pixel == numPixels - 7)){
+    Direction = -1*Direction; //Direction will be 1 or -1
+    //Change color by 77, %300 to keep within 0-299 range
+    color = (color + 77)%300;
+  }
+
+  delay(100); //controls the speed at which the pixel moves
+}
+
+//Change the if statement to make the pixel bounce between different
+//  limits
+
+// (c) 2017 Let's Start Coding. License: www.letsstartcoding.com/bsdlicense
+
